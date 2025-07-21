f3116651d1d32a8a0f1d1638ccc628cf1c80e305
Fahmi Sharif <afahmis123@gmail.com>
2025-07-21 18:14:00 +0800

    Update classroom_monitor_loop.py

--- classroom_monitor_loop.py
+++ classroom_monitor_loop.py
@@ -2,7 +2,7 @@ import time
 
 while True:
     try:
-        exec(open("/home/snowboy/Desktop/shafi&fahmi/classroom_monitor.py").read())
+        exec(open("/home/Desktop/classroom_monitor.py").read()) # change the file location to your own location
     except Exception as e:
         print(f"Error: {e}")
-    time.sleep(5)  # Wait 5 seconds
+    time.sleep(5)  # Wait 5 seconds, can change to your own setting
